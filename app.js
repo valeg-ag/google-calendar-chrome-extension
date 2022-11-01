495c9aae99feb3c3b399f0b30e70a29bacf0bd0b
avs <avs@omp-system.ru>
2022-11-01 21:55:10 +0300

    add support for YEAR view

--- app.js
+++ app.js
@@ -35,35 +35,50 @@ function isDaysEqual(d1, d2) {
     return d1.getFullYear() === d2.getFullYear() && d1.getMonth() === d2.getMonth() && d1.getDate() === d2.getDate();
 }
 
+function highlightElementIfNecessary(e, date, space){
+    if (date.getDay() === 6 || date.getDay() === 0) {
+        e.style.background = `repeating-linear-gradient( -45deg, #fce8eb, #fce8eb 3px, transparent 3px, transparent ${space})`;
+    }
+
+    for (const h of holidays) {
+        if (Array.isArray(h)) {
+            if (h[0] <= date && date <= h[1]) {
+                e.style.background = `repeating-linear-gradient( -45deg, #fce8eb, #fce8eb 3px, transparent 3px, transparent ${space})`;
+            }
+        } else {
+            if (isDaysEqual(h, date)) {
+                e.style.background = `repeating-linear-gradient( -45deg, #fce8eb, #fce8eb 3px, transparent 3px, transparent ${space})`;
+            }
+        }
+    }
+
+    for (const o of officedays) {
+        if (isDaysEqual(o, date)) {
+            e.style.background = `repeating-linear-gradient( -45deg, #cadefc, #cadefc 3px, transparent 3px, transparent ${space})`;
+        }
+    }
+}
+
 function highlightDates(e) {
     const visibleDatakeys = document.querySelectorAll("[role='main']>[data-view-heading]>[role='grid']>[role='presentation']>[role='row']>[aria-hidden='true']>div[data-datekey]");
-    if (!visibleDatakeys.length) {
+
+    const visibleYearDates = document.querySelectorAll("[data-viewkey='YEAR']>div>div>div>div>div>div>div>div>div>div>div[role='grid']>div[role='rowgroup']>div[role='row']>span[data-date]");
+
+    if (!visibleDatakeys.length && !visibleYearDates.length) {
         return;
     }
 
     for (const dateDiv of visibleDatakeys) {
         const date = datekeyToDate(dateDiv.getAttribute("data-datekey"));
-        if (date.getDay() === 6 || date.getDay() === 0) {
-            dateDiv.style.background = "repeating-linear-gradient( -45deg, #fce8eb, #fce8eb 3px, transparent 3px, transparent 20px )";
-        }
 
-        for (const h of holidays) {
-            if (Array.isArray(h)) {
-                if (h[0] <= date && date <= h[1]) {
-                    dateDiv.style.background = "repeating-linear-gradient( -45deg, #fce8eb, #fce8eb 3px, transparent 3px, transparent 20px )";
-                }
-            } else {
-                if (isDaysEqual(h, date)) {
-                    dateDiv.style.background = "repeating-linear-gradient( -45deg, #fce8eb, #fce8eb 3px, transparent 3px, transparent 20px )";
-                }
-            }
-        }
+        highlightElementIfNecessary(dateDiv, date, "20px");
+    }
 
-        for (const o of officedays) {
-            if (isDaysEqual(o, date)) {
-                dateDiv.style.background = "repeating-linear-gradient( -45deg, #cadefc, #cadefc 3px, transparent 3px, transparent 20px )";
-            }
-        }
+    for (const dateSpan of visibleYearDates) {
+        const datestr = dateSpan.getAttribute("data-date");
+        const date = new Date(parseInt(datestr.substring(0,4)), parseInt(datestr.substring(4,6))-1, parseInt(datestr.substring(6,8)))
+
+        highlightElementIfNecessary(dateSpan.firstChild, date, "3px");
     }
 }
 
